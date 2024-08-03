9f1cc956851281506ea008948e756dbc39fd3c1e
samir rain <124909580+codewithsamir@users.noreply.github.com>
2024-08-03 18:37:14 +0000

    contact us update

--- js/script.js
+++ js/script.js
@@ -2,19 +2,36 @@
 // for dark and light mode 
 const darkmodebtn = document.querySelectorAll(".dark-light-mode")
 // console.log(darkmodebtn);
-const lightmode = document.querySelector(".lightmodes")
-const darkmode = document.querySelector(".darkmodes")
+const lightmode = document.querySelectorAll(".lightmodes")
+const darkmode = document.querySelectorAll(".darkmodes")
+console.log(lightmode[1]);
+console.log(darkmode[1]);
+
 
 darkmodebtn.forEach((data)=>{
     data.addEventListener("click",()=>{
         document.body.classList.toggle("darkmode")
         if(document.body.className === "darkmode"){
-        darkmode.style.display = "none"
-            lightmode.style.display="block"
+        darkmode.forEach((dm)=>{
+            dm.style.display="none"
+        })
+        lightmode.forEach((lm)=>{
+            lm.style.display="block"
+        })
+
+
+        // darkmode.style.display = "none"
+        //     lightmode.style.display="block"
         }
         else{
-            darkmode.style.display = "block"
-            lightmode.style.display="none" 
+            // darkmode.style.display = "block"
+            // lightmode.style.display="none" 
+            darkmode.forEach((dm)=>{
+                dm.style.display="block"
+            })
+            lightmode.forEach((lm)=>{
+                lm.style.display="none"
+            })
     
         }
     })
